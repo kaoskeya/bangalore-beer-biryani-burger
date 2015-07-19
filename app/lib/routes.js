1abea731dd4cacc67f82fa4973aa3e6e6abfa47a
Karthikeyan S <keya@lostinkaos.com>
2015-07-19 11:11:53 +0530

    Fixed Route

--- app/lib/routes.js
+++ app/lib/routes.js
@@ -4,17 +4,16 @@ Router.configure({
   notFoundTemplate: 'NotFound'
 });
 
-Router.route('/:category?', {
-  name: 'home',
-  controller: 'HomeController',
+Router.route('admin', {
+  name: 'admin',
+  controller: 'AdminController',
   action: 'action',
   where: 'client'
 });
 
-
-Router.route('admin', {
-  name: 'admin',
-  controller: 'AdminController',
+Router.route('/:category?', {
+  name: 'home',
+  controller: 'HomeController',
   action: 'action',
   where: 'client'
 });
